ed87e1dd79bb88e75e8aca7ce0a649f7389ab33f
Pratina Gopali <gopali.pratina4@gmail.com>
2023-10-07 18:09:53 +0545

    project settings updated and tables created

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class DateTimeModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        auto_now=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=False,
+        auto_now=True,
+    )
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        super().save()
+    
+    
+class Artist(DateTimeModel, models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = 'Artist'
+        verbose_name_plural = 'Artists' 
+
+    def __str__(self):
+        return self.name
+    
+    
+class Song(DateTimeModel, models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    release_year = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = 'Song'
+        verbose_name_plural = 'Songs' 
+
+    def __str__(self):
+        return self.title
